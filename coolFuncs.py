acfc051d36d980efb634cff8f3a9142aa019eba3
code4sheep <jar.guinto@gmail.com>
2020-04-17 04:08:01 -0500

    trouble shooting

--- coolFuncs.py
+++ coolFuncs.py
@@ -1,4 +1,4 @@
-import math
+import math, random
 
 class Pair:
     #   used to model points and curves
@@ -39,14 +39,22 @@ def modInverse(a, m):
         x = x + m0
     return x
 
+def binarray(X):
+    list = []
+    while  X > 0:
+        #print("go")
+        list.append(X%2)
+        X = X//2
+
+    return list
 
 def ellipticAddition(P1,P2,E,N):
-    #   the work horse, does the addition according to theorm 6.6
+    #   does the addition according to theorm 6.6
     #   actually doesnt check for a - d cases
     if(P1.geta() ==  P2.geta() and P1.getb() ==  P2.getb()):
         top = (3*(P1.geta())*(P1.geta())  + E.geta()) % N
         bot = modInverse((2*P1.getb()),N)
-        print(str(top) + " " + str(bot))
+        #print(str(top) + " " + str(bot))
 
     else:
         bot = P2.geta() - P1.geta()
@@ -58,11 +66,103 @@ def ellipticAddition(P1,P2,E,N):
 
 
     lamb = (top * bot) % N
-    print("Lamb: " + str(lamb))
+    #print("Lamb: " + str(lamb))
     x3 = (lamb*lamb - P1.geta() - P2.geta()) % N
     y3 = (lamb*(P1.geta() - x3) - P1.getb()) % N
 
     ret = Pair(x3,y3)
     return ret
+
+def LECFA(N):
+    #random.randint(1,N-1)
+    A = 14
+    a = 1512
+    b = 3166
+    P = Pair(a,b)
+    B = (math.pow(b,2) - math.pow(a,3) - (A*a)) % N
+    #print("B: " + str(B))
+    E = Pair(A,B)
+    jmax = 100
+    for j in range (2,jmax):
+    #
+    #    for i in range(2,j+1):
+    #        if (i == 2):
+    #            Q = ellipticAddition(P,P,E,N)
+    #        else:
+    #            tempQ = ellipticAddition(Q,P,E,N)
+    #            if (tempQ.geta() == -1 and tempQ.getb() < 0):
+    #                Q = ellipticAddition(P,Q,E,N)
+    #            else:
+    #                Q = tempQ
+    #            if (Q.geta() == -1):
+    #                if (N > Q.getb() > 0):
+    #                    return Q.getb()
+    #                else:
+    #                    return -1
+        binlist = binarray(j)
+        print(str(binlist))
+        jbreak = []
+        Q2 = ellipticAddition(P,P,E,N)
+        #print(" | Q2: (" + str(Q2.geta()) + "," + str(Q2.getb()) + ")" )
+        flag =0
+        for i in range(0,len(binlist)):
+            if binlist[i] == 1:
+                if flag == 0:
+                    flag = 1
+                    if i == 0:
+                        Q = P
+                    else:
+                        n = pow(2,i)/2
+                        Q = doubleandadd(n,Q2,E,N)
+                else:
+                    n = pow(2, i) / 2
+                    R = doubleandadd(n, Q2, E, N)
+                    print(" | R: (" + str(R.geta()) + "," + str(R.getb()) + ")")
+                    Q = ellipticAddition(Q,R,E,N)
+        #print(" | Q: (" + str(Q.geta()) + "," + str(Q.getb()) + ")")
+        P = Q
+        print("j: " + str(j)+ " | P: (" + str(P.geta()) + "," + str(P.getb()) + ")" )
+
+
+
+
+
+
+
+def doubleandadd(n,P,E,N):
+    Q = P
+    R = Pair(0,0)
+    watch = 0
+    i = 0
+    while n > 0:
+
+        if (n % 2 == 1):
+            if (watch == 0):
+                R = Q
+                watch = 1
+                #print("flipped")
+            else:
+                #im not to sure about this but it just werks
+                temp = ellipticAddition(R,Q,E,N)
+                if(temp.geta() == -1):
+                    #print("adjusted")
+                    R = ellipticAddition(Q,R,E,N)
+                else:
+                    #print("normal")
+                    R = temp
+                #print("~Q: (" + str(Q.geta()) + "," + str(Q.getb()) + ") | R: (" + str(R.geta()) + "," + str(R.getb())+")")
+        Q = ellipticAddition(Q,Q,E,N)
+        n = n//2
+        i = i+1
+        #print("i: " + str(i) + " | n: " + str(n) + " | Q: (" + str(Q.geta()) + "," + str(Q.getb()) + ") | R: (" + str(
+        #    R.geta()) + "," + str(R.getb())+")")
+    return R
+
+
+
+
+
+
+
 
 
--- test.py
+++ test.py
@@ -1,21 +1,43 @@
 import math
-from coolFuncs import gcd, modInverse, ellipticAddition, Pair
+from coolFuncs import gcd, modInverse, ellipticAddition, Pair, doubleandadd,LECFA,binarray
 
 ans = gcd(15,9)
 
-E = Pair(3,7)
+E = Pair(14,19)
 
-P1 = Pair(43,126)
-P2 = Pair(38,112)
+R = Pair(3067,396)
+Q = Pair(0,0)
 
 
 
-pack = ellipticAddition(P2,P1,E,187)
+pack = ellipticAddition(Q,R,E,6887)
 
-if pack.geta() == -1:
-    print(str(pack.getb()) + " is a factor of " + str(187))
-else:
-    print("P3(" +str(pack.geta()) + "," + str(pack.getb())+")")
+#if pack.geta() == -1:
+#    print(str(pack.getb()) + " is a factor of " + str(187))
+#else:
+print("P3(" +str(pack.geta()) + "," + str(pack.getb())+")")
 
 print(str(ans))
 print(str(-5 % 15))
+
+n = 1
+N = 3623
+E = Pair(14,19)
+P = Pair(6,730)
+
+R = doubleandadd(n,P,E,N)
+
+print("R(" + str(R.geta()) + "," + str(R.getb()) + ")")
+
+d = LECFA(6887)
+
+print(str(d))
+
+a = binarray(2)
+
+#print(a)
+
+
+a = [[1,2],[2,1]]
+a.append([3,3])
+print (str(a[2][1]))
