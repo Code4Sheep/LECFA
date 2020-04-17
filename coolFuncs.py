8a61869abd60911e0ed2abe743af6e536fdbd8a1
code4sheep <jar.guinto@gmail.com>
2020-04-17 17:10:51 -0500

    clean up some print statments, added comments

--- coolFuncs.py
+++ coolFuncs.py
@@ -28,11 +28,6 @@ def modInverse(a, m):
     if (m == 1):
         return 0
     while (a > 1):
-
-        #i have no idea why but it fixes the problem
-        # if (m == 0):
-        #     print("black magic")
-        #     return -1
         q = a // m
         t = m
         m = a % m
@@ -45,118 +40,92 @@ def modInverse(a, m):
     return x
 
 def binarray(X):
+    #   converts int to a reversed binary array
+    #   Ex. 3 => [0,1,1]
     list = []
     while  X > 0:
-        #print("go")
         list.append(X%2)
         X = X//2
-
     return list
 
 def ellipticAddition(P1,P2,E,N):
     #   does the addition according to theorm 6.6
     #   actually doesnt check for a - d cases
     if(P1.geta() ==  P2.geta() and P1.getb() ==  P2.getb()):
+        #   P1 == P2
         top = (3*(P1.geta())*(P1.geta())  + E.geta()) % N
-        #print("look")
         bot = 2*P1.getb() % N
         if (gcd(bot,N) != 1):
-            ret = Pair(-1,bot)
+            #   check if we can invert bottom
+            #   if not return special case
+            ret = Pair(-1, bot)
             return ret
-        bot = modInverse(bot,N)
-
-        #print(str(top) + " " + str(bot))
-
+        bot = modInverse(bot, N)
     else:
+        #   P1 =/= P2
         bot = (P2.geta() - P1.geta())%N
         if (gcd(bot,N) != 1):
-            ret = Pair(-1,bot)
+            #   check if we can invert bottom
+            #   if not return special case
+            ret = Pair(-1, bot)
             return ret
-        bot = modInverse(bot,N)
+        bot = modInverse(bot, N)
         top = (P2.getb() - P1.getb()) % N
-
-
+    #   Calc the new point
     lamb = (top * bot) % N
-    #print("Lamb: " + str(lamb))
     x3 = (lamb*lamb - P1.geta() - P2.geta()) % N
     y3 = (lamb*(P1.geta() - x3) - P1.getb()) % N
-
     ret = Pair(x3,y3)
     return ret
 
 def LECFA(N):
-    #random.randint(1,N-1)
+    # implementation of factorization algorithm
     A = random.randint(1,N-1)
     a = random.randint(1,N-1)
     b = random.randint(1,N-1)
     P = Pair(a,b)
     B = (math.pow(b,2) - math.pow(a,3) - (A*a)) % N
-    print("B: " + str(B) + " " + str(A))
     E = Pair(A,B)
+    #   idk what is normal but 100 works
     jmax = 100
-    for j in range (2,jmax):
-    #
-    #    for i in range(2,j+1):
-    #        if (i == 2):
-    #            Q = ellipticAddition(P,P,E,N)
-    #        else:
-    #            tempQ = ellipticAddition(Q,P,E,N)
-    #            if (tempQ.geta() == -1 and tempQ.getb() < 0):
-    #                Q = ellipticAddition(P,Q,E,N)
-    #            else:
-    #                Q = tempQ
-    #            if (Q.geta() == -1):
-    #                if (N > Q.getb() > 0):
-    #                    return Q.getb()
-    #                else:
-    #                    return -1
+    for j in range(2, jmax):
         binlist = binarray(j)
-        print(str(binlist))
-        jbreak = []
-        Q2 = ellipticAddition(P,P,E,N)
-        print(" | Q2: (" + str(Q2.geta()) + "," + str(Q2.getb()) + ")" )
-        flag =0
-        for i in range(0,len(binlist)):
+        Q2 = ellipticAddition(P, P, E, N)
+        #   Flag notifies if its the first point
+        #   being added to Q, kinda jank but works
+        flag = 0
+        for i in range(0, len(binlist)):
+            #   iterrate through the bin array if its a one add into Q
             if binlist[i] == 1:
                 if flag == 0:
+                    #   first addition case
                     flag = 1
                     if i == 0:
                         Q = P
                     else:
+                        #   any number can be express as 2^x + 1
+                        #   not the most elegant but it works
                         n = pow(2,i)/2
-                        print(str(n))
-                        Q = doubleandadd(n,Q2,E,N)
+                        Q = doubleandadd(n, Q2, E, N)
                 else:
+                    #   just add onto existing Q
                     n = pow(2, i) / 2
-                    #print(str(n))
                     R = doubleandadd(n, Q2, E, N)
-                    if R.geta() == -1:
-                        if 1 < R.getb() < N:
-                            return gcd((-1 * R.getb()) % N, N)
-                        if R.getb() == N:
-                            return -1
-                    #print("R: (" + str(R.geta()) + "," + str(R.getb()) + ")")
-                    Q = ellipticAddition(R,Q,E,N)
-
-
-        #print(" | Q: (" + str(Q.geta()) + "," + str(Q.getb()) + ")")
+                    Q = ellipticAddition(R, Q, E, N)
         P = Q
-        print("j: " + str(j)+ " | P: (" + str(P.geta()) + "," + str(P.getb()) + ")" )
         if P.geta() == -1:
+            #   check if we have a "fail"
             if 1 < P.getb() < N:
                 return gcd((-1 * P.getb()) % N, N)
         if P.getb() == N:
+            # -1 is just the fail case
             return -1
         if (j == jmax):
             return -1
 
 
-
-
-
-
-
 def doubleandadd(n,P,E,N):
+    #   implementation of algorithm in CH 6.3.1
     Q = P
     R = Pair(0,0)
     watch = 0
@@ -164,17 +133,14 @@ def doubleandadd(n,P,E,N):
     while n > 0:
         if (n % 2 == 1):
             if (watch == 0):
+                #   if its the first then equate
                 R = Q
                 watch = 1
-                #print("flipped")
             else:
                 R = ellipticAddition(R,Q,E,N)
-                #print("~Q: (" + str(Q.geta()) + "," + str(Q.getb()) + ") | R: (" + str(R.geta()) + "," + str(R.getb())+")")
         Q = ellipticAddition(Q,Q,E,N)
         n = n//2
         i = i+1
-        #print("i: " + str(i) + " | n: " + str(n) + " | Q: (" + str(Q.geta()) + "," + str(Q.getb()) + ") | R: (" + str(
-        #    R.geta()) + "," + str(R.getb())+")")
     return R
 
 
